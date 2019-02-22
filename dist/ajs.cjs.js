72f7884bc23d3484322f7c5d927a579fe5b8bbf6
qddegtya <qddegtya@gmail.com>
2019-02-22 16:44:18 +0800

    chore: bump to 1.0.5

--- dist/ajs.cjs.js
+++ dist/ajs.cjs.js
@@ -1,4 +1,4 @@
-/** AJS (1.0.4):  💗 A collection of utility libraries used by @qddegtya*/
+/** AJS (1.0.5):  💗 A collection of utility libraries used by @qddegtya*/
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
