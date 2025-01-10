f06b440f250e959015d2bfb3bd61f2876d94cd72
qddegtya <qddegtya@gmail.com>
2025-01-10 17:24:38 +0800

    chore: update config file

--- rollup.config.js
+++ rollup.config.js
@@ -8,29 +8,29 @@ import babel from "rollup-plugin-babel";
 const LIB_NAME = `AJS`;
 const AUTHOR_NAME = `qddegtya`;
 
+const baseOutput = {
+  name: LIB_NAME,
+  banner: `/** ${LIB_NAME} (${LIB_VERSION}): ${LIB_DESCRIPTION} */`,
+  footer: `/** Follow me: @${AUTHOR_NAME} (https://github.com/${AUTHOR_NAME}) */`,
+};
+
 export default {
   input: "./src/index.js",
   output: [
     {
+      ...baseOutput,
       file: "dist/ajs.cjs.js",
       format: "cjs",
-      name: LIB_NAME,
-      banner: `/** ${LIB_NAME} (${LIB_VERSION}): ${LIB_DESCRIPTION} */`,
-      footer: `/** Follow me: @${AUTHOR_NAME} (https://github.com/${AUTHOR_NAME}) */`,
     },
     {
+      ...baseOutput,
       file: "dist/ajs.umd.js",
       format: "umd",
-      name: LIB_NAME,
-      banner: `/** ${LIB_NAME} (${LIB_VERSION}): ${LIB_DESCRIPTION} */`,
-      footer: `/** Follow me: @${AUTHOR_NAME} (https://github.com/${AUTHOR_NAME}) */`,
     },
     {
+      ...baseOutput,
       file: "dist/ajs.es.js",
       format: "es",
-      name: LIB_NAME,
-      banner: `/** ${LIB_NAME} (${LIB_VERSION}): ${LIB_DESCRIPTION} */`,
-      footer: `/** Follow me: @${AUTHOR_NAME} (https://github.com/${AUTHOR_NAME}) */`,
     },
   ],
   plugins: [resolve(), babel()],
